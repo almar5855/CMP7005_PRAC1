7c0f5513ed4f8a284c9405908e514e08167645c9
Andrew Martin <andrewlmartin@yahoo.co.uk>
2026-03-24 11:16:13 +0000

    Some extra stuff that may or may not belong in the repo, but can live there for now.

--- backend_stub.py
+++ backend_stub.py
@@ -1,14 +1,30 @@
 
-#import streamlit as st
-#import pandas as pd
+import pandas as pd
 
-#df = pd.read_csv('processed.csv')
-#df.head()
-from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
+original = pd.read_csv('original_data.csv')
+processed = pd.read_csv('processed.csv')
 
-app = FastAPI()
+def get_region_names():
+    return original['station'].unique()
 
-@app.get("/hello")
-def read_root():
-    return {"message": "Hello, World!"}
+def get_component_names():
+    return original.columns
+
+def get_data(regions=None, date_from=None, date_to=None):
+
+    if (regions is None or len(regions)==0):
+        return original
+
+    result = []
+    tmp = original.groupby('station')
+    for region in regions:
+        result.append(tmp.get_group(region))
+    result = pd.concat(result)
+
+    if date_from is not None:
+        result = result[pd.to_datetime(result['datetime']) >= pd.to_datetime(date_from)]
+
+    if date_to is not None:
+        result = result[pd.to_datetime(result['datetime']) <= pd.to_datetime(date_to)]
+
+    return result
--- test_app.py
+++ test_app.py
@@ -1,24 +1,46 @@
 
 import streamlit as st
 from streamlit_option_menu import option_menu
-import requests
+import backend_stub as bs
+import datetime
 
-BASE_URL = "http://127.0.0.1:8000"
+with st.sidebar:
+    selected = option_menu(
+        menu_title='Navigation',
+        options=['Home', 'Dataset Understanding', 'Data Preprocessing', 'Statisitical Analysis', 'Modeling'],
+        icons=['house', 'list-task', 'gear', 'graph-up', 'gear'],
+        menu_icon="cast",
+        default_index=0,
+    )
+st.title(f'{selected}')
 
-st.header("Hello")
-response = requests.get(f"{BASE_URL}/hello")
-if response.status_code == 200:
+def refresh_dataset(regions=None, date_from=None, date_to=None):
+    st.dataframe(bs.get_data(regions, date_from, date_to))
 
-    st.write(response.json())
-else:
-    st.write("Not found.")
+all_regions = bs.get_region_names()
+regions = st.multiselect('Region', all_regions)
+select_all = st.checkbox('Select all')
+if select_all:
+    regions = all_regions
+st.write(regions)
 
-# with st.sidebar:
-#     selected = option_menu(
-#         menu_title='Navigation',
-#         options=['Home', 'Dataset Understanding', 'Data Preprocessing', 'Statisitical Analysis', 'Modeling'],
-#         icons=['house', 'list-task', 'gear', 'graph-up', 'gear'],
-#         menu_icon="cast",
-#         default_index=0,
-#     )
-# st.title(f'{selected}')
+min_date = datetime.date(2013, 3, 1)
+max_date = datetime.date(2017, 2, 28)
+date_from = st.date_input("From date",
+                value=min_date,
+                min_value=min_date,
+                max_value=max_date,
+                format="YYYY.MM.DD",
+                on_change=refresh_dataset,
+)
+date_to = st.date_input("From date",
+                value=max_date,
+                min_value=date_from,
+                max_value=max_date,
+                format="YYYY.MM.DD",
+                on_change=refresh_dataset,
+)
+st.write(date_from)
+st.write(date_to)
+refresh_dataset(regions, date_from, date_to)
+
