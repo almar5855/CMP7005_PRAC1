097199e8a76cd58f92008bb4db9d2210b6ee7ce1
Andrew Martin <andrewlmartin@yahoo.co.uk>
2026-03-25 12:27:59 +0000

    Cool, those changes mostly behave as I want. Good place to stop and pick up next time.

--- backend_stub.py
+++ backend_stub.py
@@ -4,12 +4,6 @@ import pandas as pd
 original = pd.read_csv('original_data.csv')
 processed = pd.read_csv('processed.csv')
 
-def get_region_names():
-    return original['station'].unique()
-
-def get_component_names():
-    return original.columns
-
 def get_data(regions=None, date_from=None, date_to=None):
 
     if (regions is None or len(regions)==0):
@@ -28,3 +22,18 @@ def get_data(regions=None, date_from=None, date_to=None):
         result = result[pd.to_datetime(result['datetime']) <= pd.to_datetime(date_to)]
 
     return result
+
+def get_region_names():
+    return original['station'].unique()
+
+def get_component_names():
+    return original.columns
+
+def get_dataset_shape(regions=None, date_from=None, date_to=None):
+    return get_data(regions, date_from, date_to).shape
+
+def get_dataset_info(regions=None, date_from=None, date_to=None):
+    return get_data(regions, date_from, date_to).info()
+
+def get_dataset_description(regions=None, date_from=None, date_to=None):
+    return get_data(regions, date_from, date_to).describe()
--- pages/data_understanding.py
+++ pages/data_understanding.py
@@ -8,3 +8,8 @@ selected = nav.render_navbar()
 st.title(selected)
 
 regions, date_from, date_to = fc.render_filter()
+
+st.write(bs.get_dataset_shape(regions, date_from, date_to))
+st.write(bs.get_dataset_info(regions, date_from, date_to))
+st.write(bs.get_dataset_description(regions, date_from, date_to))
+
